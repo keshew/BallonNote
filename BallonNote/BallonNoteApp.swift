03cf0979114ddeef7fe78866cc4528e6df82cce4
keshew <artyom.korotckov2017@yandex.ru>
2025-06-25 14:01:52 +0500

    Initial Commit

---
+++ BallonNote/BallonNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BallonNoteApp.swift
+//  BallonNote
+//
+//  Created by Артём Коротков on 25.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BallonNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
